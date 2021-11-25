095d2aef66b83248fff98a986eb5b542986d3f4e
João Lucas Woigt Azevedo <joaowoigt@jeraapps.com>
2021-11-25 15:58:12 -0300

    Changed some behaviours

--- src/app/login-page/login-main/login-main.component.ts
+++ src/app/login-page/login-main/login-main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { EmailValidator, FormBuilder, Validators } from '@angular/forms';
 import { AppService, UserInterface } from 'src/app/app.service';
 import { Router } from '@angular/router';
 
