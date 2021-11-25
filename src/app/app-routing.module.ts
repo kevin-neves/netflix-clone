f865d8452e73562cd44439646a0a89dbc9d8f90b
Kevin Jr. Neves <kj.neves@hotmail.com>
2021-11-25 18:41:10 -0300

    v1.1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { MoviesPageComponent } from './movies-page/movies-page.component';
 
 const routes: Routes = [
-  {path: 'movies', component: MoviesPageComponent}
+  {path: 'movies', component: MoviesPageComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
